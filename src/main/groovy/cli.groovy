d0895c02465e36186e28a066d1c3c8a1d3d47456
Nishant mehta <nishantmehta.n@gmail.com>
2015-12-30 00:01:51 -0500

    fixed classpath issue for groovy script

--- src/main/groovy/cli.groovy
+++ src/main/groovy/cli.groovy
@@ -2,5 +2,7 @@ import java.text.*
 
 System.out.println("my groovy script")
 
-def cli = new CliBuilder()
-
+def cli = new CliBuilder(usage: 'slide')
+cli.app(args: 1, argName: 'type', "type ('pdf', 'deck' or 'both')", required: true)
+cli.env(args: 1, argName: 'file', 'file to convert', required: true)
+def options = cli.parse(args)
